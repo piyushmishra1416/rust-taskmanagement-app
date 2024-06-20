e1e5438b19dd6fdd130dcd579c5c0b84d5b69357
Piyush <91911367+piyushmishra1416@users.noreply.github.com.>
2024-06-21 00:17:08 +0530

    added the route to crate and get the task

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,11 @@ struct Task {
 #[derive(Deserialize)]
 struct NewUser{}
 
+#[derive(Deserialize)]
+struct NewTask{
+    description: String,
+    status: String
+}
 struct AppState {
     users: Mutex<HashMap<Uuid, User>>,
 }
@@ -37,7 +42,31 @@ async fn create_user(state: web::Data<AppState>, _user: web::Json<NewUser>) -> i
 
 }
 
+async fn create_task(state: web::Data<AppState>, user_id: web::Path<Uuid>, task: web::Json<NewTask>)-> impl Responder{
+    let mut users = state.users.lock().unwrap();
+    if let Some(user) =  users.get_mut(&user_id){
+        let new_task_id = Uuid::new_v4();
+        let new_task = Task{
+            id: new_task_id,
+            description: task.description.clone(),
+            status: task.status.clone()
+        };
+        user.tasks.push(new_task);
+        HttpResponse::Created().json(user.tasks.last().unwrap().clone())
+    }else{
+        HttpResponse::NotFound().body("User not found")
+    }
+}
 
+async fn get_tasks(state: web::Data<AppState>, user_id: web::Path<Uuid>) -> impl Responder {
+    let users = state.users.lock().unwrap();
+    if let Some(user) = users.get(&user_id){
+        HttpResponse::Ok().json(&user.tasks)
+    }
+    else{
+        HttpResponse::NotFound().body("User not found")
+    }
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -51,6 +80,8 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(state.clone())
             .route("/users", web::post().to(create_user))
+            .route("/users/{user_id}/tasks", web::post().to(create_task))
+            .route("/users/{user_id}/tasks", web::get().to(get_tasks))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
